c1b2e5a04e38a931a4e7aabe1d7ac9055bffc99d
dongwooBae <dw5817@naver.com>
2025-06-12 21:42:26 +0900

    init

---
+++ src/main/java/com/sumkim/sumkimwelfarecenterspring/SumkimWelfareCenterSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sumkim.sumkimwelfarecenterspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumkimWelfareCenterSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SumkimWelfareCenterSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sumkim/sumkimwelfarecenterspring/SumkimWelfareCenterSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumkim.sumkimwelfarecenterspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumkimWelfareCenterSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
